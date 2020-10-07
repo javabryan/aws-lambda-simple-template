0956789b9ce2ce18ba708132ed18b67301638956
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2020-10-07 09:24:43 -0500

    Add files via upload

---
+++ simple-lambda-template.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Close dialog with the customer, reporting fulfillmentState of Failed or Fulfilled
+function close(sessionAttributes, fulfillmentState, message) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Close',
+            fulfillmentState,
+            message,
+        },
+    };
+}
+
+// --------------- Events -----------------------
+function dispatch(intentRequest, callback) {
+    const sessionAttributes = intentRequest.sessionAttributes;
+
+    callback(close(sessionAttributes, 'Fulfilled',
+        { 'contentType': 'PlainText', 'content': `This is where I should be listing categories` }));
+
+}
+
+// --------------- Main handler -----------------------
+
+// Route the incoming request based on intent.
+// The JSON body of the request is provided in the event slot.
+exports.handler = (event, context, callback) => {
+    try {
+        dispatch(event,
+            (response) => {
+                callback(null, response);
+            });
+    } catch (err) {
+        callback(err);
+    }
+};
